165cf77c06c835fe89e384aa7417b7d6e74e6f29
hasanain2000 <hasne2001@gmail.com>
2024-12-14 00:18:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,11 +7,14 @@ curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 
 # Start Tailscale
-sudo tailscale up --ssh --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS} || sudo tailscale up --ssh --auth-key=${TAILSCALE_AUTHKEY} --hostname=render-vpn
---advertise-exit-node  ${TAILSCALE_ADDITIONAL_ARGS}
+sudo tailscale up --ssh --auth-key=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME} --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS} || sudo tailscale up --ssh --auth-key=${TAILSCALE_AUTHKEY} --hostname=render-vpn --advertise-exit-node ${TAILSCALE_ADDITIONAL_ARGS}
 
 # Enable SSH
 # sudo tailscale up --ssh
+
+# Start a simple HTTP server on port 80 for health check
+# The server listens on port 80 and responds with a 200 OK to /health
+python3 -m http.server 80 &
 
 # Keep the container running
 exec sleep infinity
